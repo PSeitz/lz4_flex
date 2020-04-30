d0383ec4ea675e0d69b3e7315ccb4707a157d228
Pascal Seitz <pascal.seitz@gmail.com>
2020-05-01 00:47:40 +0200

    asdf

--- src/bin/profile_decomp.rs
+++ src/bin/profile_decomp.rs
@@ -1,18 +1,18 @@
-// extern crate lz4_flex;
+extern crate lz4_flex;
 
 
 
-// // const COMPRESSION10MB: &'static [u8] = include_bytes!("../../benches/dickens.txt");
-// const COMPRESSION10MB: &'static [u8] = include_bytes!("../../benches/compression_66k_JSON.txt");
+// const COMPRESSION10MB: &'static [u8] = include_bytes!("../../benches/dickens.txt");
+const COMPRESSION10MB: &'static [u8] = include_bytes!("../../benches/compression_66k_JSON.txt");
 
-// fn main() {
+fn main() {
 
-//     use cpuprofiler::PROFILER;
-//     let compressed = lz4_flex::compress(COMPRESSION10MB as &[u8]);
-//     PROFILER.lock().unwrap().start("./my-prof.profile").unwrap();
-//     for _ in 0..1000 {
-//         lz4_flex::decompress(&compressed).unwrap();
-//     }
-//     PROFILER.lock().unwrap().stop().unwrap();
+    // use cpuprofiler::PROFILER;
+    // let compressed = lz4_flex::compress(COMPRESSION10MB as &[u8]);
+    // PROFILER.lock().unwrap().start("./my-prof.profile").unwrap();
+    // for _ in 0..1000 {
+    //     lz4_flex::decompress(&compressed).unwrap();
+    // }
+    // PROFILER.lock().unwrap().stop().unwrap();
     
-// }
+}
