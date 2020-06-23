e2aa6067d7510ab267d0ab3867777e8da1bc5c26
Pascal Seitz <pascal.seitz@sap.com>
2020-06-23 21:29:19 +0200

    Update Docs, publish version 0.1.4 to npm

--- lz4-wasm/example_project/index.js
+++ lz4-wasm/example_project/index.js
@@ -1,6 +1,6 @@
 import * as wasm from "lz4-wasm";
 
-// use TextEncoder tro get bytes from string
+// use TextEncoder to get bytes (UInt8Array) from string
 var enc = new TextEncoder();
 const compressed = wasm.compress(enc.encode("compress this text, compress this text pls. thx. thx. thx. thx. thx"));
 const original = wasm.decompress(compressed);
