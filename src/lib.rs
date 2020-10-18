d1596eca8e420bf94d14180262032e7dfb6d5db8
Pascal Seitz <pascal.seitz@gmail.com>
2020-10-18 16:35:44 +0200

    switch docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
-//! Pure Rust, high performance implementation of LZ4 compression.
-//!
-//! A detailed explanation of the algorithm can be found [here](http://ticki.github.io/blog/how-lz4-works/).
-//!
-//!
-//! # Examples
-//! ```
-//! use lz4_flex::compress_prepend_size;
-//! use lz4_flex::decompress_size_prepended;
-//! let input = "test data".as_bytes();
-//! let compressed = compress_prepend_size(input);
-//! let uncompressed = decompress_size_prepended(&compressed).unwrap();
-//! assert_eq!(input, uncompressed);
-//!	
-//!	## Feature Flags
-//!	There are two feature flags: default = safe-encode and safe-decode 
-//!	safe-decode is enabled by default. Currently it adds more checks to the unsafe code, but it still uses unsafe.
-//!	
-//!	safe-encode will switch the compression to completely safe rust code.
-//! ```
+/*! Pure Rust, high performance implementation of LZ4 compression.
+
+A detailed explanation of the algorithm can be found [here](http://ticki.github.io/blog/how-lz4-works/).
+
+
+# Examples
+```
+use lz4_flex::{compress_prepend_size, decompress_size_prepended};
+let input: &[u8] = b"Hello people, what's up?";
+let compressed = compress_prepend_size(input);
+let uncompressed = decompress_size_prepended(&compressed).unwrap();
+assert_eq!(input, uncompressed);
+
+# Feature Flags
+There are two feature flags: default = safe-encode and safe-decode 
+safe-decode is enabled by default. Currently it adds more checks to the unsafe code, but it still uses unsafe.
+
+safe-encode will switch the compression to completely safe rust code.
+```
+*/
 extern crate byteorder;
 #[macro_use]
 extern crate quick_error;
