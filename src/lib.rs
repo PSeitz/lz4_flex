55aada7c1c55d45b4841937195ad2d76ff8e54f8
Pascal Seitz <pascal.seitz@gmail.com>
2020-10-18 17:20:45 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,8 @@ assert_eq!(input, uncompressed);
 ```
 
 ## Feature Flags
-There are two feature flags: default = safe-encode and safe-decode
+There are two feature flags: safe-encode and safe-decode.
+
 safe-decode is enabled by default. Currently it adds more checks to the unsafe code, but it still uses unsafe.
 
 safe-encode will switch the compression to completely safe rust code.
