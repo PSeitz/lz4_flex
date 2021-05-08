203359c1110ad86264bb8b0f8c57f3839982920c
Arthur Silva <arthurprs@gmail.com>
2021-05-08 11:30:29 +0200

    Compile and run each fuzzer for 10s in CI

--- fuzz/fuzz_targets/fuzz_roundtrip_cpp_compress.rs
+++ fuzz/fuzz_targets/fuzz_roundtrip_cpp_compress.rs
@@ -5,6 +5,8 @@ fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
     let mut compressed = Vec::new();
     lzzzz::lz4::compress_to_vec(data, &mut compressed, lzzzz::lz4::ACC_LEVEL_DEFAULT).unwrap();
-    let decompressed = lz4_flex::decompress(&compressed, data.len()).unwrap();
-    assert_eq!(data, decompressed.as_slice());
+    if !compressed.is_empty() {
+        let decompressed = lz4_flex::decompress(&compressed, data.len()).unwrap();
+        assert_eq!(data, decompressed.as_slice());
+    }
 });
