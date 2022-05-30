3f562dd5123a5043175ec4089c3d5b6b70db8fd9
Pascal Seitz <pascal.seitz@gmail.com>
2022-05-30 21:32:08 +0800

    fix doctest

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -10,7 +10,7 @@
 //! let stdin = io::stdin();
 //! let stdout = io::stdout();
 //! let mut rdr = stdin.lock();
-//! Wrap the stdout writer in a LZ4 Frame writer.
+//! // Wrap the stdout writer in a LZ4 Frame writer.
 //! let mut wtr = lz4_flex::frame::FrameEncoder::new(stdout.lock());
 //! io::copy(&mut rdr, &mut wtr).expect("I/O operation failed");
 //! wtr.finish().unwrap();
