7d2185af05524fdb98e5edf0e8057e9680cd5453
Philip Ye <yestyle@gmail.com>
2023-01-29 23:10:09 +1300

    Add a test case for legacy frames decompression
    `benches/dickens.lz4` was generated by cli utility lz4:
    
    lz4 -l benches/dickens.txt benches/dickens.lz4

--- tests/tests.rs
+++ tests/tests.rs
@@ -16,6 +16,7 @@ const COMPRESSION34K: &[u8] = include_bytes!("../benches/compression_34k.txt");
 const COMPRESSION65: &[u8] = include_bytes!("../benches/compression_65k.txt");
 const COMPRESSION66JSON: &[u8] = include_bytes!("../benches/compression_66k_JSON.txt");
 const COMPRESSION10MB: &[u8] = include_bytes!("../benches/dickens.txt");
+const DECOMPRESSION10MB: &[u8] = include_bytes!("../benches/dickens.lz4");
 
 fn lz4_cpp_block_compress(input: &[u8]) -> Result<Vec<u8>, lzzzz::Error> {
     let mut out = Vec::new();
@@ -574,6 +575,12 @@ mod frame {
             }
             r => panic!("{:?}", r),
         }
+    }
+
+    #[test]
+    fn legacy_frame() {
+        let uncompressed = lz4_flex_frame_decompress(DECOMPRESSION10MB).unwrap();
+        assert_eq!(uncompressed, COMPRESSION10MB);
     }
 }
 
