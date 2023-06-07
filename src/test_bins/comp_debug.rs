2f31931bd3353eb1a87f2ed098d48400b98b4748
Pascal Seitz <pascal.seitz@gmail.com>
2023-06-07 23:05:49 +0800

    cleanup

--- src/test_bins/comp_debug.rs
+++
@@ -1,8 +0,0 @@
-extern crate lz4_flex;
-
-fn main() {
-    let s = r#"There is nothing either good or bad, but thinking makes it so."#;
-
-    let compressed = lz4_flex::compress(s.as_bytes());
-    lz4_flex::decompress(&compressed, s.len()).unwrap();
-}
