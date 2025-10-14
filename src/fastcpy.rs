ad71a3103daa37f8ddd139f0c29b6e4ce8724ba3
Clement Rey <cr.rey.clement@gmail.com>
2025-10-14 12:50:07 +0200

    fix illegal doc comment

--- src/fastcpy.rs
+++ src/fastcpy.rs
@@ -58,7 +58,7 @@ pub fn slice_copy(src: &[u8], dst: &mut [u8]) {
         return;
     }
 
-    /// The code will use the vmovdqu instruction to copy 32 bytes at a time.
+    // The code will use the vmovdqu instruction to copy 32 bytes at a time.
     #[cfg(target_feature = "avx")]
     {
         if len <= 64 {
